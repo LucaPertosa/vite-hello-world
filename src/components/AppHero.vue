e42b72b3dfb7f1bb6af8b0fa8322417312ca8e0a
LucaPertosa <luca.pertosa@hotmail.it>
2023-04-12 14:08:45 +0200

    Add: AppTitle.vue & AppHero.vue

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+            image: "../src/assets/img/01.png",
+            altTxt: "Vite + Vue image"
+        }
+    }
+}
+</script>
+
+<template>
+    <img :src="image" :alt="altTxt">
+</template>
+
+<style>
+img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+}
+</style>
